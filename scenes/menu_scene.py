baef2082a8e0149d35a144364ddabdb4565e8efd
An Dae Hun <dkseogns88@naver.com>
2024-11-22 03:10:54 +0900

    버튼이미지변경

--- scenes/menu_scene.py
+++ scenes/menu_scene.py
@@ -5,16 +5,24 @@ from scenes.scene import Scene
 class Menu_Scene(Scene):
     def __init__(self):
         self.background = pico2d.load_image('resource/menu_background.png')
-        self.start_button = pico2d.load_image('resource/start_button.png')
-        self.exit_button = pico2d.load_image('resource/exit_button.png')
+        self.start_button = pico2d.load_image('resource/startbutton.png')
+        self.exit_button = pico2d.load_image('resource/exitbutton.png')
         self.start_button_pos = (512, 300)
         self.exit_button_pos = (512, 100)
 
     def draw(self):
         pico2d.clear_canvas()
         self.background.draw(512, 512)
-        self.start_button.draw(*self.start_button_pos)
-        self.exit_button.draw(*self.exit_button_pos)
+        button_width = self.start_button.w // 2  # 가로 크기 절반
+        button_height = self.start_button.h // 2  # 세로 크기 절반
+        self.start_button.clip_draw(0, 0, self.start_button.w, self.start_button.h,
+                                    self.start_button_pos[0], self.start_button_pos[1],
+                                    button_width, button_height)
+
+        self.exit_button.clip_draw(0, 0, self.exit_button.w, self.exit_button.h,
+                                   self.exit_button_pos[0], self.exit_button_pos[1],
+                                   button_width, button_height)
+
         pico2d.update_canvas()
 
     def handle_events(self, events):
